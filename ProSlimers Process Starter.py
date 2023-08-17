97475ec6f6d4663cd3f54dbad867eaf604d9517d
ProSlimer <76175042+ProSlimer@users.noreply.github.com>
2023-08-16 22:44:46 -0500

    GUI Update
    Added a GUI for selecting options such as Program, File Path, and Delay. Will continue to develop the GUI in the future

--- ProSlimers Process Starter.py
+++ ProSlimers Process Starter.py
@@ -1,12 +1,47 @@
+#ProSlimer's Process Starter
+
+#I probably don't need all of these, but I can't remember why I added them and I am too lazy to figure it out
 from cProfile import run
 from concurrent.futures import process
 from multiprocessing.sharedctypes import Value
 import os
-from sys import path_hooks
 from urllib import response
 import psutil
 import time
 from datetime import datetime
+import PySimpleGUI as sg
+import subprocess
+
+#GUI Elements:
+sg.theme('Dark Grey 13')
+layout = [[sg.Text('Program Name')],
+          [sg.Input()],
+          [sg.Text('Executable Location')],
+          [sg.Input(), sg.FileBrowse()],
+          [sg.Text('Check Frequency (In seconds)')],
+          [sg.Input()],
+          [sg.OK('Start'), sg.Cancel()]]
+
+window = sg.Window("ProSlimer's Process Starter", layout)
+
+#GUI Logic:
+while True:
+    #Read data from the input fields of the window
+    event, values = window.read()
+    #If the "Start" button is pressed, pass the data and delete the window
+    if event == 'Start':
+        window.close(); del window
+        break 
+    #If the window is closed or the "Cancel" button is pressed, quit the program
+    elif event ==sg.WIN_CLOSED or event == 'Cancel':
+        quit()
+
+#Set named variables to list items returned from the GUI window
+program = values[0]
+path = values[1]
+delay = int(values[2])
+
+print ("DEBUG:\nPROGRAM:",program, "\nPATH:",path ,"\nDELAY:",delay) #DEBUG: PATH VERIFICATON
 
 def checkIfProcessRunning(program):
 
@@ -34,12 +69,9 @@ def runprogram():
         os.startfile(path)
     #Log that the file was started
         print(program, "seems to have stopped, restarting");
-        
-program = input("What is the name of the program we are looking for?\n")
-path = input("And where is the file located? (Full path)\n")
-delay = int(input("How often would you like to check? (in seconds; checking more often increases resource usage)\n"))
-print('Process starter is now looking for', program, 'in', path, 'every', delay, 'seconds.\n')
-#Start infinate loop
+
+print("Process Starter Has Started")
+#Set value = True to start infinate loop
 while (True): 
     runprogram() 
     time.sleep (delay) 
